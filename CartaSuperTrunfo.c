519a636e1945584ab9282a122e6fa1914c35c85e
llorrainean <lorrainean31@gmail.com>
2025-03-20 23:00:46 +0000

    re1

--- CartaSuperTrunfo.c
+++ CartaSuperTrunfo.c
@@ -16,13 +16,13 @@ int main() {
     printf("\nDigite os Dados da Carta 1\n");
 
     printf("Estado: (Ex:SP)\n ");
-    scanf("%c", estado);
+    scanf("%2s", estado);
 
     printf("Código da Cidade: \n");
-    scanf("%c", codigo);
+    scanf("%2s", codigo);
 
     printf("Nome da Cidade: \n");
-    scanf("%c", cidade);
+    scanf("%2s", cidade);
 
     printf("População: \n");
     scanf("%d", populacao);
@@ -38,9 +38,9 @@ int main() {
 
     // Exibindo os Dados
     printf("\n DADOS DA CIDADE \n");
-    printf("Estado: %c\n", estado);
-    printf("Código: %c\n", codigo);
-    printf("Cidade: %c\n", cidade);
+    printf("Estado: %2s\n", estado);
+    printf("Código: %2s\n", codigo);
+    printf("Cidade: %2s\n", cidade);
     printf("População: %d\n", populacao);
     printf("Área: %f km²\n", area);
     printf("PIB: R$ %f bilhões\n", pib);
